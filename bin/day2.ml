e7de9e2182389a72a754a50baef775445c9c2645
Krzysztof Bielikowicz <kbielikowicz98@gmail.com>
2024-12-02 23:54:34 +0000

    Day 2 part 2

--- bin/day2.ml
+++ bin/day2.ml
@@ -1,67 +1,83 @@
 open Core
+open Stdio
 
 module Report = struct
-  (*type t = level list*)
-  (*and level = int*)
+  (** Represents a sequence of reactor levels *)
+  type t = int list
 
-  type mode = Increasing | Decreasing
+  (** Direction of change between consecutive levels *)
+  type mode =
+    | Increasing
+    | Decreasing
 
-  type pairwise_accum = { pairs: (int * int) list; last_num: int option }
+  (** Parse a raw [line] into a report *)
+  let parse_raw line : t =
+      line |> String.split ~on:' ' |> List.map ~f:int_of_string
 
-  let pairwise list =
-    let result = List.fold list ~init:{ pairs = []; last_num = None} ~f:(fun ({ pairs; last_num } as acc) elem ->
-      match last_num with
-      | None -> { acc with last_num = Some elem }
-      | Some prev -> { pairs = (prev, elem) :: pairs; last_num = Some elem }
-    ) in
-    List.rev result.pairs
 
-  let parse_raw line =
-    line |> String.split ~on:' ' |> List.map ~f:int_of_string
-
-  let is_safe report =
-    (* keep track of current mode *)
-    let current_mode : (mode option) ref = ref None in
-    let pairs = pairwise report in
-    List.for_all pairs ~f:(fun (fst, snd) ->
-      let diff_raw = snd - fst in
-
-      (* if the number is the same we can just short-circuit, it's invalid *)
-      if diff_raw = 0 then
-        false
-      else begin
-        let diff = abs diff_raw in
-        let valid_diff = diff >= 1 && diff <= 3 in
-        let valid_mode = match !current_mode with
-          (* first time, initialize mode *)
-          | None -> begin
-              current_mode := if diff_raw > 0 then Some Increasing else Some Decreasing;
-              true
+  (** Checks if [report] is safe *)
+  let is_safe (report : t) =
+    let rec aux mode = function
+      | a :: b :: tl ->
+        let diff_raw = b - a in
+        (* if the number is the same, fail *)
+        if diff_raw = 0
+        then false
+        else begin
+          let diff = abs diff_raw in
+          let valid_diff = diff >= 1 && diff <= 3 in
+          (* if the diff is not valid, fail *)
+          if not valid_diff
+          then false
+          else begin
+            match mode with
+            | None -> begin
+              let new_mode = if b > a then Some Increasing else Some Decreasing in
+              aux new_mode (b :: tl)
+            end
+            | Some Increasing -> b > a && aux mode (b :: tl)
+            | Some Decreasing -> b < a && aux mode (b :: tl)
           end
-          (* if we're increasing every diff needs to be positive *)
-          | Some Increasing -> diff_raw > 0
-          (* if we're decreasing every diff needs to be negative *)
-          | Some Decreasing -> diff_raw < 0
-        in
-        valid_diff && valid_mode
-      end
-    )
+        end
+      (* Matches empty list or single element list, both are fine *)
+      | _ -> true
+    in
+
+    aux None report
+  ;;
+
+  (** Create a [report] variant [without] a given level *)
+  let create_variant (report : t) ~without : t =
+    List.filteri report ~f:(fun idx _ -> idx <> without)
+  ;;
+
+  (** Checks if [report] is safe, allowing one bad level *)
+  let is_safe_dampened (report : t) =
+    if is_safe report
+    then true
+    else begin
+      let max_idx = List.length report in
+
+      (** Check if report is safe while removing the element at [idx] *)
+      let rec try_removing_at idx =
+        if idx > max_idx
+        then false
+        else begin
+          let variant = create_variant report ~without:idx in
+          is_safe variant || try_removing_at (idx + 1)
+        end
+      in
+
+      try_removing_at 0
+    end
+  ;;
 end
 
 let () =
   let lines = Stdio.In_channel.read_lines "./inputs/day2_prod.txt" in
   let reports = List.map lines ~f:Report.parse_raw in
+  print_endline "Safe reports:";
   List.count reports ~f:Report.is_safe |> string_of_int |> print_endline;
-  (*List.iter reports ~f:(fun report ->*)
-  (*    print_endline "Report:";*)
-  (*    Report.pairwise report |> List.iter ~f:(fun (fst, snd) ->*)
-  (*      Stdlib.print_char '(';*)
-  (*      Stdlib.print_int fst;*)
-  (*      Stdlib.print_char ',';*)
-  (*      Stdlib.print_int snd;*)
-  (*      Stdlib.print_char ')';*)
-  (*    );*)
-  (*    print_endline ("Is valid: " ^ string_of_bool (Report.is_safe report));*)
-  (*    print_endline "";*)
-  (*);*)
-
+  print_endline "Safe reports with Problem Dampening:";
+  List.count reports ~f:Report.is_safe_dampened |> string_of_int |> print_endline
+;;

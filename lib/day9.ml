34d45981731cc51a7d1161ceef238520044bd945
Krzysztof Bielikowicz <kbielikowicz98@gmail.com>
2024-12-14 18:21:11 +0000

    Day 9 part 2

--- lib/day9.ml
+++ lib/day9.ml
@@ -7,12 +7,48 @@ let findi_last ?(from = None) array ~f =
   loop @@ Option.value from ~default:(Array.length array - 1)
 ;;
 
+let findi_last_dyn ?(from = None) (array : 'a Dynarray.t) ~f =
+  let rec loop i =
+    if i < 0
+    then None
+    else if f i (Dynarray.get array i)
+    then Some (i, Dynarray.get array i)
+    else loop (i - 1)
+  in
+  loop @@ Option.value from ~default:(Dynarray.length array - 1)
+;;
+
 let findi_from ?(from = None) array ~f =
   let len = Array.length array - 1 in
   let rec loop i =
     if i > len then None else if f i array.(i) then Some (i, array.(i)) else loop (i + 1)
   in
   loop @@ Option.value from ~default:0
+;;
+
+let findi_from_dyn ?(from = None) (array : 'a Dynarray.t) ~f =
+  let len = Dynarray.length array - 1 in
+  let rec loop i =
+    if i > len
+    then None
+    else if f i (Dynarray.get array i)
+    then Some (i, Dynarray.get array i)
+    else loop (i + 1)
+  in
+  loop @@ Option.value from ~default:0
+;;
+
+(** Insert a new [value] into [arr] at given [idx]. Shifts elements to the right to accomodate *)
+let insert_at (arr : 'a Dynarray.t) (idx : int) (value : 'a) : unit =
+  (* Add new element at end first *)
+  Dynarray.add_last arr value;
+  (* Shift elements right starting from the end *)
+  let len = Dynarray.length arr in
+  for i = len - 1 downto idx + 1 do
+    Dynarray.set arr i (Dynarray.get arr (i - 1))
+  done;
+  (* Put new value in desired position *)
+  Dynarray.set arr idx value
 ;;
 
 module DiskBlock = struct
@@ -47,6 +83,11 @@ module DiskEntry = struct
 
   let file ~size ~id = File { size; id }
   let space ~size = Space { size }
+
+  let get_size = function
+    | Space { size } -> size
+    | File { size; _ } -> size
+  ;;
 
   let to_blocks = function
     | File { size; id } ->
@@ -111,12 +152,81 @@ module M = struct
     (* run the moving logic until we can't anymore *)
     ignore @@ move (Array.length block_arr - 1) 0;
 
-    (*Array.iter block_arr ~f:(fun b -> print_endline @@ DiskBlock.show b);*)
     block_arr |> Array.to_list |> DiskBlock.checksum |> string_of_int |> print_endline
   ;;
 
   (* Run part 2 with parsed inputs *)
-  let part2 _ = ()
+  let part2 entries =
+    let entry_arr = Dynarray.create () in
+    Dynarray.append_list entry_arr entries;
+
+    let rec move file_idx =
+      let open Option.Let_syntax in
+      (* find next file, from end of arr *)
+      let%bind next_file_idx, next_file =
+        findi_last_dyn entry_arr ~from:(Some file_idx) ~f:(fun _ el ->
+          match el with
+          | DiskEntry.Space { size = _ } -> false
+          | _ -> true)
+      in
+
+      match next_file with
+      | DiskEntry.File { size = file_size; _ } -> begin
+        (* now consider ALL whitespaces, find first one with matching size *)
+        let space_result =
+          findi_from_dyn entry_arr ~f:(fun _ el ->
+            match el with
+            | DiskEntry.Space { size } -> size >= file_size
+            | _ -> false)
+        in
+
+        (* if we there is no matching space for that file anywhere, keep going *)
+        match space_result with
+        | None -> move (next_file_idx - 1)
+        | Some (next_space_idx, next_space) -> begin
+          (* if the matchin space is to the right of the file, keep going *)
+          if next_space_idx >= next_file_idx
+          then move (next_file_idx - 1)
+          else (
+            let next_size = DiskEntry.get_size next_space in
+
+            (* if the size was larger than the file *)
+            if next_size > file_size
+            then (
+              (* in place of file, put space of size of file *)
+              Dynarray.set entry_arr next_file_idx (DiskEntry.space ~size:file_size);
+
+              (* in place of space, put file + leftover space *)
+              Dynarray.set entry_arr next_space_idx next_file;
+              (* inserts a new elem *)
+              insert_at
+                entry_arr
+                (next_space_idx + 1)
+                (DiskEntry.space ~size:(next_size - file_size)))
+            else (
+              (* swap *)
+              Dynarray.set entry_arr next_space_idx next_file;
+              Dynarray.set entry_arr next_file_idx next_space);
+
+            (* recurse *)
+            move next_file_idx)
+        end
+      end
+      (* technically we always find a file but type system doesn't know that *)
+      | _ -> None
+    in
+
+    (* run the moving logic until we can't anymore *)
+    ignore @@ move (Dynarray.length entry_arr - 1);
+
+    entry_arr
+    |> Dynarray.to_list
+    |> List.map ~f:DiskEntry.to_blocks
+    |> List.join
+    |> DiskBlock.checksum
+    |> string_of_int
+    |> print_endline
+  ;;
 end
 
 include M
@@ -129,4 +239,9 @@ let example = "2333133121414131402"
 let%expect_test "part 1" =
   run example ~only_part1:true;
   [%expect {| 1928 |}]
+;;
+
+let%expect_test "part 2" =
+  run example ~only_part2:true;
+  [%expect {| 2858 |}]
 ;;
